1a844dea6aceb8bd5f646b11ed7df5c3f78fc03c
alexbesch <besch040@umn.edu>
2023-11-14 13:26:40 -0600

    Class containing Player Mecahnics

--- PlayerClasses.py
+++ PlayerClasses.py
@@ -12,26 +12,26 @@ SKIP = "SKIP"
 
 
 
-# class OthelloPlayerTemplate:
-#     '''Template class for an Othello Player
-#
-#     An othello player *must* implement the following methods:
-#
-#     get_color(self) - correctly returns the agent's color
-#
-#     make_move(self, state) - given the state, returns an action that is the agent's move
-#     '''
-#
-#     def __init__(self, mycolor):
-#         self.color = mycolor
-#
-#     def get_color(self):
-#         return self.color
-#
-#     def make_move(self, state):
-#         '''Given the state, returns a legal action for the agent to take in the state
-#         '''
-#         return None
+class OthelloPlayerTemplate:
+    '''Template class for an Othello Player
+
+    An othello player *must* implement the following methods:
+
+    get_color(self) - correctly returns the agent's color
+
+    make_move(self, state) - given the state, returns an action that is the agent's move
+    '''
+
+    def __init__(self, mycolor):
+        self.color = mycolor
+
+    def get_color(self):
+        return self.color
+
+    def make_move(self, state):
+        '''Given the state, returns a legal action for the agent to take in the state
+        '''
+        return None
 
 
 class RandomPlayer:
@@ -83,3 +83,48 @@ class AlphaBetaPlayer:
         value, move = max_value_alpha_beta(state, self.depth_limit, self.color, float('-inf'), float('inf'))
 
         return move
+
+class HumanPlayer(OthelloPlayerTemplate):
+    def __init__(self, mycolor):
+        self.color = mycolor
+
+    def get_color(self):
+        return self.color
+
+    def make_move(self, state):
+        colorName = "WHITE"
+        if self.color == -1:
+            colorName = "BLACK"
+
+        print("#############  ", colorName, "  #############")
+        print("Before move, current utility: ", utilityFunction(state, self.color))
+
+        curr_move = None
+        legals = actions(state)
+        while curr_move == None:
+            display(state)
+            if self.color == 1:
+                print("White ", end='')
+            else:
+                print("Black ", end='')
+            print(" to play.")
+            print("Utility function is: ", utilityFunction(state, self.color))
+            print("Legal moves are " + str(legals))
+            move = input("Enter your move as a r,c pair:")
+            if move == "":
+                return legals[0]
+
+            if move == SKIP and SKIP in legals:
+                return move
+
+            try:
+                movetup = int(move.split(',')[0]), int(move.split(',')[1])
+            except:
+                movetup = None
+            if movetup in legals:
+                curr_move = movetup
+            else:
+                print("That doesn't look like a legal action to me")
+
+            print("After move, current utility: ", utilityFunction(result(state, curr_move), self.color))
+        return curr_move
