280ac41246b48153af8d4c0e7e0babec056ca8d7
Mohit Yadav <mohitydv09@gmail.com>
2023-12-17 21:28:38 -0600

    copy

---
+++ gameuttt.py
@@ -0,0 +1,276 @@
+# Importing libraries
+import copy
+from copy import deepcopy
+import time
+import heuristics as heu  # Importing a module named 'heuristics' as 'heu'
+
+# Defining Global Variables.
+EMPTY = '-'  # Symbol representing an empty cell on the game board
+X = 'X'      # Symbol representing Player X
+O = "O"      # Symbol representing Player O
+T = "T"      # Symbol representing a Tie (when the game ends with no winner)
+SIZE = 9      # Total number of mini-boards in the game
+SIZEMINI = 3  # Size of each mini-board
+WINNING_POSITIONS = [  # List of tuples representing winning positions on the mini-boards
+    ((0, 0), (0, 1), (0, 2)),
+    ((1, 0), (1, 1), (1, 2)),
+    ((2, 0), (2, 1), (2, 2)),
+    ((0, 0), (1, 0), (2, 0)),
+    ((0, 1), (1, 1), (2, 1)),
+    ((0, 2), (1, 2), (2, 2)),
+    ((0, 0), (1, 1), (2, 2)),
+    ((0, 2), (1, 1), (2, 0))
+]
+MAXSCORE = 10000  # Maximum score value for the game
+print_player_moves = False  # Flag to control whether player moves should be printed
+
+# Global variables list
+global_vars = [EMPTY, X, O, T, SIZE, SIZEMINI, WINNING_POSITIONS, MAXSCORE]
+
+import players  # Importing a module named 'players'
+
+class UtttState:
+    '''A class to represent a state in Ultimate Tic-Tac-Toe game.'''
+
+    # Constructor method to initialize the state
+    def __init__(self, currentplayer, otherplayer, last_move=None, board_array=None, num_moves=0):
+        if board_array != None:
+            self.board_array = board_array
+        else:
+            # Creating an empty 3D array to represent the game board
+            self.board_array = [[[EMPTY] * SIZEMINI for _ in range(SIZEMINI)] for _ in range(SIZE)]
+        self.current = currentplayer
+        self.other = otherplayer
+        self.last_move = last_move
+        self.master_board = build_master(self.board_array)  # Building the master board
+        self.num_moves = num_moves
+        # self.heuristic = heuristic(self, currentplayer.sign)
+
+    # String representation of the state
+    def __repr__(self) -> str:
+        new_string = "Last Action Chosen: " + str(self.last_move) + "     Player Turn: " + str(
+            self.current.get_sign()) + "\n"
+        new_string += "-------------------------------------\n"
+        for m in range(SIZEMINI):
+            for j in range(SIZEMINI):
+                new_string += "|  "
+                for i in range(SIZEMINI):
+                    for k in range(SIZEMINI):
+                        new_string += self.board_array[i + (3 * m)][j][k] + "  "
+                    new_string += "|  "
+                new_string += "\n"
+            new_string += "-------------------------------------\n"
+
+        new_string += " ## MASTER BOARD ##\n"
+        new_string += "-------------\n"
+        for i in range(SIZEMINI):
+            new_string += "|  "
+            for j in range(SIZEMINI):
+                new_string += self.master_board[i][j] + "  "
+            new_string += "|  "
+            new_string += "\n"
+        new_string += "-------------\n"
+        # new_string += "Current Player: " + str(self.current.get_sign()) + "    Heuristic: " + str(self.heuristic) + "\n"
+        return new_string
+
+
+class UtttGame:
+    """A class to encapsulate the variable and methods for the Ultimate Tic-Tac-Toe game."""
+    pass  # Placeholder class with no attributes or methods
+
+def terminal_test(state):
+    # INPUT: 3X3 GAME BOARD
+    # OUTPUT: BOOLEAN IF GAME IS WON OR NOT, CHARACTER OF WINNING PLAYER
+    return terminal_test_mini(state.master_board)
+
+def terminal_test_mini(mini_board):
+    """Terminal test for Mini Boards. Input is the 3*3 array and outputs whether the game has ended and the character who won or 'T' if tied."""
+    for positions in WINNING_POSITIONS:
+        a, b, c = positions
+        if (mini_board[a[0]][a[1]] != EMPTY) and (mini_board[a[0]][a[1]] != T):
+            if (mini_board[a[0]][a[1]] == mini_board[b[0]][b[1]] == mini_board[c[0]][c[1]]):
+                return True, mini_board[a[0]][a[1]]
+    ## It might be the case that a mini board has tied.
+    ## Now check for a tie. In our game, a tie only happens if all the board pieces have been filled, and there is yet no winner.
+    for i in range(SIZEMINI):
+        for j in range(SIZEMINI):
+            if mini_board[i][j] == EMPTY:
+                # If any empty place is found, that means we have not Tied.
+                return False, None
+    ## We reached here means the board is not won yet, and it is a Tie.
+    return True, T
+
+def build_master(game):
+    """This is a time-consuming function should be used with caution."""
+    # INPUT: 9X3X3 MASTER GAME BOARD
+    # OUTPUT: SMALLER 3X3 GAME BOARD REPRESENTING LARGER BOARD
+    return_game = [[EMPTY] * SIZEMINI for _ in range(SIZEMINI)]
+    for i in range(SIZE):
+        mini_board = game[i]
+        game_won, winner = terminal_test_mini(mini_board)
+        if game_won:
+            return_game[i // 3][i % 3] = winner
+    return return_game
+
+## Change Legal actions to account for when the selected board is full,
+#  and hence the player can move anywhere on the board.
+def actions(state):
+    """Return a list of possible actions possible from this state (Instance of UtttState).
+    Actions look like (i, x, y), where i is the mini board number and x, y
+    represent the row and column of the place in the mini-board."""
+    legal_actions = []
+
+    ## If this is the first move.
+    if state.last_move == None:
+        # All actions are legal.
+        # 'mb' stands for mini-board,
+        for mb in range(SIZE):
+            for i in range(SIZEMINI):
+                for j in range(SIZEMINI):
+                    legal_actions.append((mb, i, j))
+    else:
+        # There was some last move.
+        last_mb, last_i, last_j = state.last_move
+        # Find the new legal mb from the last i, j.
+        mb = 3 * last_i + last_j
+        # We need to check that this mb is empty in the master board, or else we need to give all other open boards as legal moves.
+        if state.master_board[mb // 3][mb % 3] == EMPTY:
+            for i in range(3):
+                for j in range(3):
+                    if state.board_array[mb][i][j] == EMPTY:
+                        legal_actions.append((mb, i, j))
+        else:
+            # This is when the master_board has a Tie or Has been won.
+            # Add all mb to the possible boards.
+            possible_mb = []
+            for i in range(SIZEMINI):
+                for j in range(SIZEMINI):
+                    if state.master_board[i][j] == EMPTY:
+                        possible_mb.append(3 * i + j)
+            for mb in possible_mb:
+                for i in range(SIZEMINI):
+                    for j in range(SIZEMINI):
+                        if state.board_array[mb][i][j] == EMPTY:
+                            legal_actions.append((mb, i, j))
+    return legal_actions
+
+
+
+def result(state, action):
+    """Returns the resulting state after taking the given action.
+    Returns None if the action is not legal."""
+    # Get the position of move from action.
+    mb, i, j = action
+    ## Check for legal action, by checking that the position is empty.
+    if state.board_array[mb][i][j] != EMPTY:
+        return None
+    else:
+        # New state has to be formed here.
+        # Make the new Board array.
+        new_board = deepcopy(state.board_array)
+        new_board[mb][i][j] = state.current.sign
+        new_state = UtttState(state.other, state.current, last_move=action, board_array=new_board, num_moves=state.num_moves + 1)
+        return new_state
+
+
+def search_test(player1, player2, num_iterations=1, printouts=True):
+    # Function to conduct a series of game simulations and evaluate player performance
+    score = [0, 0, 0]  # [player1, player2, tie]
+    ts = time.time()
+    for i in range(num_iterations):
+        print("Iteration: ", i + 1, "/", num_iterations, "  Score: ", score, "[P1, P2, Tie]")
+        winner = play_game(player1, player2, printouts)
+        if winner == player1.get_sign():
+            score[0] += 1
+        elif winner == player2.get_sign():
+            score[1] += 1
+        elif winner == T:
+            score[2] += 1
+        else:
+            raise Exception("Winner not recognized")
+    t_stop = time.time()
+    print("Final score: [P1, P2, Tie]", score)
+    print("Computational time: ", t_stop - ts, "s    Average: ", (t_stop - ts) / num_iterations, "s")
+    return score
+
+
+def play_game(p1=None, p2=None, printouts=True):
+    """Play the game with two players. Default use two humans."""
+    # Function to simulate a game between two players
+    if p1 == None:
+        p1 = players.HumanPlayer(X)
+    if p2 == None:
+        p2 = players.HumanPlayer(O)
+
+    s = UtttState(p1, p2)
+    while True:
+    # for i in range(20):
+        action = p1.make_move(s)
+        if action not in actions(s):
+            # print("Illegal move made by X")
+            # print("O wins!")
+            return None
+        s = result(s, action)
+        if printouts: print(s)
+        game_over, winner = terminal_test(s)
+        if game_over:
+            print("Game Over")
+            print("Player " + winner + " wins!")
+            if not printouts: print(s)
+            return winner
+        action = p2.make_move(s)
+        if action not in actions(s):
+            # print("Illegal move made by O")
+            # print("O wins!")
+            return None
+        # print(s)
+        s = result(s, action)
+        if printouts: print(s)
+        game_over, winner = terminal_test(s)
+        if game_over:
+            print("Game Over")
+            print("Player " + winner + " wins!")
+            if not printouts: print(s)
+            return winner
+    # print('Debug here.')
+    # # print(s)
+    # p1.heuristic(s)
+
+
+
+def main():
+    # HEURISTIC OPTIONS
+    # Name - Max_tested_working_depth - designer
+    # heu.homemade - Designed and made by Mohit/Alex
+    # heu.pulkit_github - Designed and made by Pulkit (From uttt github python repo)
+    # heu.homemadeV2 - Designed and made by Mohit/Alex, gained inspiration from fulkit_github
+
+    # PLAYER OPTIONS
+    # Name - designer
+    # players.HumanPlayer - Designed and made by Mohit/Alex
+    # players.AlphaBetaPlayer - Designed and made by Mohit/Alex
+    # players.RandomPlayer - Designed and made by Mohit/Alex
+
+    # p1 = players.AlphaBetaPlayer(O, 6, heu.homemadeV2)
+    # p2 = players.HumanPlayer(X)
+    # play_game(p1, p2)
+
+
+    # CODE USED IN THE FINAL TESTING OF THE ALGORITHMS AND HEURISTICS
+    p1 = players.AlphaBetaPlayer(O, 6, heu.homemadeV2)
+    p2 = players.AlphaBetaPlayer(X, 6, heu.pulkit_github)
+    iters = 1
+    print("Running homemadeV2 vs PulkitGithub test")
+    ts = time.time()
+    score = search_test(p1, p2, iters, printouts=False)
+    t_stop = time.time()
+    print("Score: [random_wins, homemade_wins, ties", score)
+    # print("Total time: ", t_stop - ts, "s")
+
+
+
+if __name__ == '__main__':
+    main()
+
+
+
---
+++ players.py
@@ -0,0 +1,283 @@
+### Implement the player Classes Here.
+# Import the other file funtions here, check this at end.
+import gameuttt as game
+from random import choice
+import heuristics as heu
+
+
+# Defining Global Variables.
+# Global variables are defined in gameuttt.py
+
+# GLOBAL VARIABLES - Defined in gameuttt.py
+# global_vars = [EMPTY, X, O, T, SIZE, SIZEMINI, WINNING_POSITIONS]
+
+EMPTY = game.global_vars[0]
+X = game.global_vars[1]
+O = game.global_vars[2]
+T = game.global_vars[3]
+SIZE = game.global_vars[4]
+SIZEMINI = game.global_vars[5]
+WINNING_POSITIONS = game.global_vars[6]
+MAXSCORE = game.global_vars[7]
+
+class UtttPlayerTemplate:
+    """Template class for UTTT player classes. This is inherited by all classes."""
+
+    def __init__(self, mysign) -> None:
+        self.sign = mysign
+
+    def get_sign(self):
+        return self.sign
+
+    def make_move(self, state):
+        """Given a state, return a action that the agent will take.
+          Action should be legal.
+          This will be diffrent for each player,
+          therefore, should be implemented in each class seperately"""
+        return None
+
+
+class RandomPlayer(UtttPlayerTemplate):
+    """Randomly playing agent for the game."""
+
+    def make_move(self, state):
+        legal_actions = game.actions(state)
+        # print("Legal Actions:",legal_actions)
+        move = choice(legal_actions)
+        if game.print_player_moves: print(f"Random Player made move: {move}")
+        return move
+
+
+class HumanPlayer(UtttPlayerTemplate):
+    def make_move(self, state):
+        curr_move = None
+        legal_moves = game.actions(state)
+        print(state)
+        while curr_move == None:
+            if self.sign == 'X':
+                print("X", end='')
+            else:
+                print("O", end='')
+            print(' to play.')
+            # print("[DEBUGGING] Current Player: ", self.sign, "Current Heuristic Value: ", heu.pulkit_github(self, state))
+            print(f'Legal Moves are : {legal_moves}')
+            input_move = input(
+                'Enter your move as mb,r,c pair. mb is the board number, r, c are row and column index inside the mb where you want to make the move:\n')
+            if input_move == '':
+                print('No move was provided. Hence first move from above list was selected.')
+                return legal_moves[0]
+            else:
+                try:
+                    move = tuple(map(int, input_move.split(',')))
+                except ValueError:
+                    print('Enter Integer Values.')
+                    continue
+                if len(move) != 3:
+                    print("Not a valid move.")
+                    continue
+                else:
+                    if (move[0] < 9 and move[0] >= 0) or (move[1] < 3 and move[1] >= 0) or (
+                            move[2] < 3 and move[2] >= 0):
+                        curr_move = move
+                    else:
+                        print("Not a valid Move.")
+                        continue
+        return curr_move
+
+
+class MinimaxPlayer(UtttPlayerTemplate):
+    def __init__(self, mysign, depth_limit, heuristic):
+        self.depth_limit = depth_limit
+        self.sign = mysign
+        self.heuristicfcn = heuristic
+
+    def make_move(self, state):
+        if state.num_moves < 2:
+            legal_actions = game.actions(state)
+            # print("Legal Actions:",legal_actions)
+            move = choice(legal_actions)
+            if game.print_player_moves: print("Took Random Move: ", state.num_moves)
+        else:
+            move = self.minimax_search(state)
+        if game.print_player_moves: print(f"Minimax Player made move: {move}")
+        return move
+
+    def minimax_search(self, state):
+        current_depth = 0
+        value, move = self.max_value(state, current_depth)
+        return move
+
+    def max_value(self, state, current_depth):
+        # Returns  (utility, move)
+        if game.terminal_test(state)[0]:
+            return self.heuristic(state), None
+        # Check if depth limit has been reached
+        if self.depth_limit == current_depth:
+            return self.heuristic(state), None
+
+        current_depth += 1
+        v = float('-inf')
+
+        for action in game.actions(state):
+            result_state = game.result(state, action)
+            v2, a2 = self.min_value(result_state, current_depth)
+            if v2 > v:
+                v, move = v2, action
+        return v, move
+
+    def min_value(self, state, current_depth):
+        # Returns (utility, move)
+        if game.terminal_test(state)[0]:
+            return self.heuristic(state), None
+        # Check if depth limit has been reached.
+        if self.depth_limit == current_depth:
+            return self.heuristic(state), None
+
+        current_depth += 1
+        v = float('inf')
+
+        for action in game.actions(state):
+            result_state = game.result(state, action)
+            v2, a2 = self.max_value(result_state, current_depth)
+            if v2 < v:
+                v, move = v2, action
+        return v, move
+
+    def heuristic(self, state):
+        return self.heuristicfcn(self, state)
+
+
+
+class AlphaBetaPlayer(UtttPlayerTemplate):
+    def __init__(self, mysign, depth_limit, heuristic):
+        self.depth_limit = depth_limit
+        self.sign = mysign
+        self.heuristicfcn = heuristic
+
+    def make_move(self, state):
+        if state.num_moves < 2:
+            legal_actions = game.actions(state)
+            # print("Legal Actions:",legal_actions)
+            move = choice(legal_actions)
+            if game.print_player_moves: print("Took Random Move: ", state.num_moves)
+        else:
+            move = self.alpha_beta_search(state)
+        if game.print_player_moves: print(f"AlphaBeta Player made move: {move}")
+        return move
+
+    def alpha_beta_search(self, state):
+        # Returns a action that the person has to do.
+        current_depth = 0
+        value, move = self.max_value(state, float('-inf'), float('inf'), current_depth)
+        return move
+
+    def max_value(self, state, alpha, beta, current_depth):
+        # Returns (utility, move)
+        if game.terminal_test(state)[0]:
+            return self.heuristic(state), None
+        if self.depth_limit == current_depth:
+            return self.heuristic(state), None
+
+        current_depth += 1
+        v = float('-inf')
+
+        for action in game.actions(state):
+            result_state = game.result(state, action)
+            v2, a2 = self.min_value(result_state, alpha, beta, current_depth)
+            if v2 > v:
+                v, move = v2, action
+                alpha = max(alpha, v)
+            if v >= beta:
+                return v, move
+        return v, move
+
+    def min_value(self, state, alpha, beta, current_depth):
+        # Returns (utility, move)
+        if game.terminal_test(state)[0]:
+            return self.heuristic(state), None
+        if self.depth_limit == current_depth:
+            return self.heuristic(state), None
+
+        current_depth += 1
+        v = float('+inf')
+
+        for action in game.actions(state):
+            result_state = game.result(state, action)
+            v2, a2 = self.max_value(result_state, alpha, beta, current_depth)
+            if v2 < v:
+                v, move = v2, action
+                beta = min(beta, v)
+            if v <= alpha:
+                return v, move
+        return v, move
+
+    def heuristic(self, state):
+        return self.heuristicfcn(self, state)
+
+
+
+class AlphaBetaPlayerV2(UtttPlayerTemplate):
+    def __init__(self, mysign, depth_limit, heuristic):
+        self.depth_limit = depth_limit
+        self.sign = mysign
+        self.heuristicfcn = heuristic
+
+    def make_move(self, state):
+        if state.num_moves < 2:
+            legal_actions = game.actions(state)
+            # print("Legal Actions:",legal_actions)
+            move = choice(legal_actions)
+            if game.print_player_moves: print("Took Random Move: ", state.num_moves)
+        else:
+            move = self.alpha_beta_search(state)
+        if game.print_player_moves: print(f"AlphaBetaV2 Player made move: {move}")
+        return move
+
+    def alpha_beta_search(self, state):
+        # Returns a action that the person has to do.
+        current_depth = 0
+        value, move = self.max_value(state, float('-inf'), float('inf'), current_depth)
+        return move
+
+    def max_value(self, state, alpha, beta, current_depth):
+        # Returns (utility, move)
+        if game.terminal_test(state)[0]:
+            return self.heuristic(state), None
+        if self.depth_limit == current_depth:
+            return self.heuristic(state), None
+
+        current_depth += 1
+        v = float('-inf')
+
+        for action in game.actions(state):
+            result_state = game.result(state, action)
+            v2, a2 = self.min_value(result_state, alpha, beta, current_depth)
+            if v2 > v:
+                v, move = v2, action
+                alpha = max(alpha, v)
+            if v >= beta:
+                return v, move
+        return v, move
+
+    def min_value(self, state, alpha, beta, current_depth):
+        # Returns (utility, move)
+        if game.terminal_test(state)[0]:
+            return self.heuristic(state), None
+        if self.depth_limit == current_depth:
+            return self.heuristic(state), None
+
+        current_depth += 1
+        v = float('+inf')
+
+        for action in game.actions(state):
+            result_state = game.result(state, action)
+            v2, a2 = self.max_value(result_state, alpha, beta, current_depth)
+            if v2 < v:
+                v, move = v2, action
+                beta = min(beta, v)
+            if v <= alpha:
+                return v, move
+        return v, move
+
+    def heuristic(self, state):
+        return self.heuristicfcn(self, state)
