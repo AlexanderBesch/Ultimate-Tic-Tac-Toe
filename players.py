dc4886dc0f98aedf29c6e395673508a9ea2e8be9
alexbesch <besch040@umn.edu>
2023-12-18 16:55:17 -0600

    Class containing Player Mecahnics

--- players.py
+++ players.py
@@ -85,7 +85,7 @@ class HumanPlayer(UtttPlayerTemplate):
 
 
 class MinimaxPlayer(UtttPlayerTemplate):
-    def __init__(self, mysign, depth_limit, heuristic):
+    def __init__(self, mysign, depth_limit=4, heuristic=heu.heuristic2):
         self.depth_limit = depth_limit
         self.sign = mysign
         self.heuristicfcn = heuristic
