1ec24c07c8ad790e87463781097495b366e9a7e3
Mohit Yadav <mohitydv09@gmail.com>
2023-12-18 16:24:49 -0600

    minor change

--- gameuttt.py
+++ gameuttt.py
@@ -44,7 +44,6 @@ class UtttState:
         self.last_move = last_move
         self.master_board = build_master(self.board_array)  # Building the master board
         self.num_moves = num_moves
-        # self.heuristic = heuristic(self, currentplayer.sign)
 
     # String representation of the state
     def __repr__(self) -> str:
@@ -251,22 +250,21 @@ def main():
     # players.AlphaBetaPlayer - Designed and made by Mohit/Alex
     # players.RandomPlayer - Designed and made by Mohit/Alex
 
-    # p1 = players.AlphaBetaPlayer(O, 6, heu.homemadeV2)
-    # p2 = players.HumanPlayer(X)
-    # play_game(p1, p2)
+    p1 = players.AlphaBetaPlayer(O, 6, heu.homemadeV2)
+    p2 = players.HumanPlayer(X)
+    play_game(p1, p2)
 
 
     # CODE USED IN THE FINAL TESTING OF THE ALGORITHMS AND HEURISTICS
-    p1 = players.AlphaBetaPlayer(O, 6, heu.homemadeV2)
-    p2 = players.AlphaBetaPlayer(X, 6, heu.pulkit_github)
-    iters = 1
-    print("Running homemadeV2 vs PulkitGithub test")
-    ts = time.time()
-    score = search_test(p1, p2, iters, printouts=False)
-    t_stop = time.time()
-    print("Score: [random_wins, homemade_wins, ties", score)
+    # p1 = players.AlphaBetaPlayer(O, 6, heu.homemadeV2)
+    # p2 = players.AlphaBetaPlayer(X, 6, heu.pulkit_github)
+    # iters = 1
+    # print("Running homemadeV2 vs PulkitGithub test")
+    # ts = time.time()
+    # score = search_test(p1, p2, iters, printouts=False)
+    # t_stop = time.time()
+    # print("Score: [random_wins, homemade_wins, ties", score)
     # print("Total time: ", t_stop - ts, "s")
-
 
 
 if __name__ == '__main__':
